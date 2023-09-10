22b1f7bddfd0a8417f2fe41a30f9ff705f27bf6d
Ibrahim abboud <coraynova@hotmail.com>
2023-09-10 14:25:30 +0300

    Featured changed from static to dynamic

--- app/Components/Home/FeaturedGames.jsx
+++ app/Components/Home/FeaturedGames.jsx
@@ -1,45 +1,34 @@
-import Image from 'next/image'
 import styles from './FeaturedGames.module.css'
+import Link from 'next/link'
+import featuredGames from '../../api/gamesale.json'
 
 export default function FeaturedGames(){
+    const games = []
+    featuredGames.forEach(game => {
+        if(game.featured){
+            games.push(game)
+        }
+    });
     return(
         <div className={styles.container}>
             <div className={styles.title}>Featured PC Games</div>
 
             <div className={styles.cards}>
-                <div className={styles.card}>
-                    <div className={styles.image}>
-                        <img 
-                            src={'https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767w.jpg'}
-                        />
-                        <div className={styles.cover}>
-                            <img src='https://media.contentapi.ea.com/content/dam/battlefield/battlefield-2042/common/season-01/bf-2042-white-nav-logo.svg'/>
-                            <p>Battlefield 2042</p>
-                        </div>
+                {games.map(game => (
+                    <div className={styles.card} key={game.id}>
+                        <Link href={`/games/${game.id}`}>
+                            <div className={styles.image}>
+                                <img 
+                                    src={game.image}
+                                />
+                                <div className={styles.cover}>
+                                    <img src={game.logo}/>
+                                    <p>{game.title}</p>
+                                </div>
+                            </div>
+                        </Link>
                     </div>
-                </div>
-                <div className={styles.card}>
-                    <div className={styles.image}>
-                        <img 
-                            src={'https://media.contentapi.ea.com/content/dam/gin/images/2022/05/1-1-gamebox-f122-keyart-1.png.adapt.crop1x1.767w.png'}
-                        />
-                        <div className={styles.cover}>
-                            <img src='https://media.contentapi.ea.com/content/dam/gin/common/logos/f122-gamebox-logo.png'/>
-                            <p>F1® 22</p>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.card}>
-                    <div className={styles.image}>
-                        <img 
-                            src={'https://media.contentapi.ea.com/content/dam/gin/images/2022/07/eas-fifa22-gen5-ue-keyart-horz-f23logo.jpg.adapt.crop1x1.767w.jpg'}
-                        />
-                        <div className={styles.cover}>
-                            <img src='https://media.contentapi.ea.com/content/dam/gin/common/logos/fifa23-logo-white-stacked.svg'/>
-                            <p>FIFA 23</p>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     )
